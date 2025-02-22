be539255fe45cc1aeec65b696d32b04e3243d208
Uxugin <git@uxugin.com>
2025-02-22 14:37:51 -0700

    hunt tags

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,52 @@ pub const MAJOR: u8 = 0;
 pub const MINOR: u8 = 1;
 pub const PATCH: u8 = 0;
 pub const PRE: u8 = 0;
+pub mod tags {
+    pub const IGNORE_NEXT: u8 = unsafe { core::mem::transmute(i8::MIN) };
+    pub const NODES_START: u8 = unsafe { core::mem::transmute(1i8) };
+    pub const NODES_END: u8 = unsafe { core::mem::transmute(-1i8) };
+}
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum ErrorDecode {
     LayoutBroken,
     MagicNumbers,
     Version,
 }
-pub fn read_file(data: Vec<u8>) -> Result<(), ErrorDecode> {
+fn hunt_tags(data: &[u8], start: u8, end: u8) -> Vec<&[u8]> {
+    let mut skip_next = false;
+    let mut inside = 0u8;
+    let mut sections = Vec::<&[u8]>::new();
+    let mut current_section_start: Option<usize> = None;
+    for i in 0..data.len() {
+        let byte = data[i];
+        if skip_next {
+            skip_next = false;
+            continue;
+        }
+        if byte == tags::IGNORE_NEXT {
+            skip_next = true;
+            continue;
+        }
+        if byte == start {
+            if inside == 0 {
+                current_section_start = Some(i + 1);
+            }
+            inside += 1;
+        }
+        if byte == end && inside > 0 {
+            inside -= 1;
+            if inside == 0 {
+                let real_current_section_start = current_section_start.unwrap();
+                if real_current_section_start < i {
+                    sections.push(&data[real_current_section_start..i]);
+                }
+                current_section_start = None;
+            }
+        }
+    }
+    sections
+}
+pub fn read_file(data: &Vec<u8>) -> Result<(), ErrorDecode> {
     if data.len() < 16 {
         return Err(ErrorDecode::LayoutBroken);
     }
@@ -33,5 +72,16 @@ pub fn read_file(data: Vec<u8>) -> Result<(), ErrorDecode> {
     if pre > PRE {
         return Err(ErrorDecode::Version);
     }
+    hunt_tags(&data[16..], tags::NODES_START, tags::NODES_END);
     Ok(())
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn hunt_tags_() {
+        let data = vec![1, 100, 2, 3, 101, 4, 100, 5, 6, 7, 101, 8];
+        let found = hunt_tags(&data, 100, 101);
+        assert_eq!(found, vec![&[2, 3][..], &[5, 6, 7][..]]);
+    }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -2,20 +2,20 @@ use rrtk_rsb::*;
 #[test]
 fn read_file_layout_broken() {
     let file = b"rrtkstrmbldrvvv";
-    assert_eq!(read_file(file.into()), Err(ErrorDecode::LayoutBroken));
+    assert_eq!(read_file(&file.into()), Err(ErrorDecode::LayoutBroken));
 }
 #[test]
 fn read_file_magic_number_error() {
     let file = b"badmagicnumsvvvv";
-    assert_eq!(read_file(file.into()), Err(ErrorDecode::MagicNumbers));
+    assert_eq!(read_file(&file.into()), Err(ErrorDecode::MagicNumbers));
 }
 #[test]
 fn read_file_version_error() {
     let file: [u8; 16] = unsafe { core::mem::transmute((*b"rrtkstrmbldr", [u8::MAX; 4])) };
-    assert_eq!(read_file(file.into()), Err(ErrorDecode::Version));
+    assert_eq!(read_file(&file.into()), Err(ErrorDecode::Version));
 }
 #[test]
 fn read_file_correct() {
     let file: [u8; 16] = unsafe { core::mem::transmute((*b"rrtkstrmbldr", [0u8, 1, 0, 0])) };
-    read_file(file.into()).unwrap();
+    read_file(&file.into()).unwrap();
 }
