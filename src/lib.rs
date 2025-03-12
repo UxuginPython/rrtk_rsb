3b14c0a06ee5582f226022527ed51477e3d27496
Uxugin <git@uxugin.com>
2025-03-12 00:40:05 -0600

    kinda sorta compliant now

--- src/lib.rs
+++ src/lib.rs
@@ -42,9 +42,51 @@ mod tags_u8 {
 }
 #[derive(Clone, Debug, PartialEq)]
 pub struct Node {
+    pub id: u16,
     pub x: f64,
     pub y: f64,
     pub inputs: Vec<u16>,
+}
+fn hunt_tag(data: &[u8], tag: u8) -> Option<&[u8]> {
+    let mut skip_next = 0u32;
+    for i in 0..data.len() {
+        if skip_next >= 1 {
+            skip_next -= 1;
+            continue;
+        }
+        let byte = data[i];
+        let mut continuing = true;
+        skip_next = match byte {
+            tags_u8::SKIP_1 => 1,
+            tags_u8::SKIP_2 => 2,
+            tags_u8::SKIP_4 => 4,
+            tags_u8::SKIP_8 => 8,
+            tags_u8::SKIP_16 => 16,
+            tags_u8::SKIP_U8 => {
+                //We want to skip the next byte, the one telling us how far to skip, itself, as well as
+                //adding a 'bias value' of 1 since skipping 0 bytes is worthless and it lets us skip up
+                //to 256 instead of 255.
+                data[i + 1] as u32 + 2
+            }
+            tags_u8::SKIP_U16 => {
+                //Similarly to SKIP_U8, we skip the next two bytes (the ones telling us how far to
+                //skip) and a bias of 1.
+                (unsafe { transmute::<[u8; 2], u16>([data[i + 1], data[i + 2]]) }) as u32 + 3
+            }
+            _ => {
+                continuing = false;
+                0
+            }
+        };
+        if continuing {
+            continue;
+        }
+        //Everything above here is copied from hunt_tags.
+        if byte == tag {
+            return Some(&data[i..data.len()]);
+        }
+    }
+    return None;
 }
 fn hunt_tags(data: &[u8], start: u8, end: u8) -> Vec<&[u8]> {
     let mut skip_next = 0u32;
@@ -52,11 +94,11 @@ fn hunt_tags(data: &[u8], start: u8, end: u8) -> Vec<&[u8]> {
     let mut sections = Vec::<&[u8]>::new();
     let mut current_section_start: Option<usize> = None;
     for i in 0..data.len() {
-        let byte = data[i];
         if skip_next >= 1 {
             skip_next -= 1;
             continue;
         }
+        let byte = data[i];
         let mut continuing = true;
         skip_next = match byte {
             tags_u8::SKIP_1 => 1,
@@ -104,23 +146,68 @@ fn hunt_tags(data: &[u8], start: u8, end: u8) -> Vec<&[u8]> {
 }
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 struct ErrorParseNode;
-fn parse_node(data: &[u8]) -> Result<Node, ErrorParseNode> {
-    if data.len() < 17 || data.len() % 2 != 1 || data[0] != tags_u8::SKIP_16 {
+fn find_and_parse_node_id(data: &[u8]) -> Result<u16, ErrorParseNode> {
+    let found = hunt_tag(data, tags_u8::NODE_ID);
+    let found = match found {
+        Some(x) => x,
+        None => return Err(ErrorParseNode),
+    };
+    //TODO: make this follow the rule that you can use [SKIP_U8 2u8] and [SKIP_U16 2u16] too
+    if found[1] != tags_u8::SKIP_2 {
         return Err(ErrorParseNode);
     }
-    let x: [u8; 8] = [
-        data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8],
-    ];
-    let x: f64 = unsafe { transmute(x) };
-    let y: [u8; 8] = [
-        data[9], data[10], data[11], data[12], data[13], data[14], data[15], data[16],
-    ];
-    let y: f64 = unsafe { transmute(y) };
-    let mut inputs = Vec::<u16>::new();
-    for i in 0..(data.len() - 17) / 2 {
-        inputs.push(unsafe { transmute::<[u8; 2], u16>([data[i * 2 + 17], data[i * 2 + 18]]) });
+    return Ok(unsafe { transmute([found[2], found[3]]) });
+}
+fn find_and_parse_coordinates(data: &[u8]) -> Result<(f64, f64), ErrorParseNode> {
+    let found = hunt_tag(data, tags_u8::COORDINATES);
+    let found = match found {
+        Some(x) => x,
+        None => return Err(ErrorParseNode),
+    };
+    //TODO: make this allow more options for how the numbers are escaped
+    if found[1] != tags_u8::SKIP_16 {
+        return Err(ErrorParseNode);
     }
+    Ok((
+        unsafe {
+            transmute([
+                found[2], found[3], found[4], found[5], found[6], found[7], found[8], found[9],
+            ])
+        },
+        unsafe {
+            transmute([
+                found[10], found[11], found[12], found[13], found[14], found[15], found[16],
+                found[17],
+            ])
+        },
+    ))
+}
+//TODO: make this able to error in case a bad thing happens
+fn find_and_parse_inputs(data: &[u8]) -> Vec<u16> {
+    let input_section = hunt_tags(
+        data,
+        tags_u8::NODE_INPUT_LIST_START,
+        tags_u8::NODE_INPUT_LIST_END,
+    );
+    if input_section.len() == 0 {
+        return Vec::new();
+    }
+    if input_section.len() != 1 {
+        panic!();
+    }
+    let input_section = input_section[0];
+    let mut inputs = Vec::<u16>::new();
+    for i in 0..(input_section.len() / 2) {
+        inputs.push(unsafe { transmute([input_section[i * 2], input_section[i * 2 + 1]]) });
+    }
+    inputs
+}
+fn parse_node(data: &[u8]) -> Result<Node, ErrorParseNode> {
+    let id = find_and_parse_node_id(data)?;
+    let (x, y) = find_and_parse_coordinates(data)?;
+    let inputs = find_and_parse_inputs(data);
     Ok(Node {
+        id: id,
         x: x,
         y: y,
         inputs: inputs,
