f51485c0fc28d2ae593a882e87d2417aaff95874
Uxugin <git@uxugin.com>
2025-03-15 10:20:03 -0600

    fix tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -4,17 +4,17 @@ use rrtk_rsb::*;
 #[test]
 fn read_file_layout_broken() {
     let file = b"rrtkstrmbldrvvv";
-    assert_eq!(read_file(&file.into()), Err(ErrorDecode::LayoutBroken));
+    assert_eq!(read_file(&file.into()), Err(error::ParseFile::LayoutBroken));
 }
 #[test]
 fn read_file_magic_number_error() {
     let file = b"badmagicnumsvvvv";
-    assert_eq!(read_file(&file.into()), Err(ErrorDecode::MagicNumbers));
+    assert_eq!(read_file(&file.into()), Err(error::ParseFile::MagicNumbers));
 }
 #[test]
 fn read_file_version_error() {
     let file: [u8; 16] = unsafe { core::mem::transmute((*b"rrtkstrmbldr", [u8::MAX; 4])) };
-    assert_eq!(read_file(&file.into()), Err(ErrorDecode::Version));
+    assert_eq!(read_file(&file.into()), Err(error::ParseFile::Version));
 }
 #[test]
 fn read_file_no_node_section() {
