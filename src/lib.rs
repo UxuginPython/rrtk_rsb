aeb30572ad5c043b13d916031524633055943ec0
Uxugin <git@uxugin.com>
2025-03-23 08:06:03 -0600

    NodeType enum

--- src/lib.rs
+++ src/lib.rs
@@ -40,6 +40,50 @@ mod tags_u8 {
     pub const NODE_INPUT_LIST_START: u8 = unsafe { transmute(tags::NODE_INPUT_LIST_START) };
     pub const NODE_INPUT_LIST_END: u8 = unsafe { transmute(tags::NODE_INPUT_LIST_END) };
 }
+#[repr(u16)]
+pub enum NodeType {
+    ConstantGetter = 0,
+    NoneGetter = 1,
+    Expirer = 2,
+    Latest = 3,
+    CommandPID = 4,
+    EWMAStream = 5,
+    MovingAverageStream = 6,
+    PIDControllerStream = 7,
+    PositionToState = 8,
+    VelocityToState = 9,
+    AccelerationToState = 10,
+    NoneToError = 11,
+    NoneToValue = 12,
+    FloatToQuantity = 13,
+    QuantityToFloat = 14,
+    DimensionAdder = 15,
+    DimensionRemover = 16,
+    FreezeStream = 17,
+    IfStream = 18,
+    IfElseStream = 19,
+    AndStream = 20,
+    OrStream = 21,
+    NotStream = 22,
+    SumStream = 23,
+    Sum2 = 24,
+    DifferenceStream = 25,
+    ProductStream = 26,
+    Product2 = 27,
+    QuotientStream = 28,
+    ExponentStream = 29,
+    DerivativeStream = 30,
+    IntegralStream = 31,
+}
+impl TryFrom<u16> for NodeType {
+    type Error = ();
+    fn try_from(was: u16) -> Result<NodeType, ()> {
+        if was <= 31 {
+            return Ok(unsafe { transmute(was) });
+        }
+        Err(())
+    }
+}
 #[derive(Clone, Debug, PartialEq)]
 pub struct Node {
     pub id: u16,
